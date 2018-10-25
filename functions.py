42f094681380f34a52ea3ba66c5ba62c6d717118
root <root@DESKTOP-D6C8D28.localdomain>
2018-10-25 18:27:31 +0530

    Template_Matching_Done

--- functions.py
+++ functions.py
@@ -21,7 +21,7 @@ def threshold_otsu_method(img_gray):
 def extract_contour(thresholded_img):
 	img_edges = cv.Canny(thresholded_img,20,120, apertureSize=3)
 	plt.imshow(img_edges, cmap='gray', interpolation='nearest')
-	plt.savefig('img.jpg')
+	plt.savefig('img_contour.jpg')
 	return img_edges
 	
 def contours(im):
@@ -68,11 +68,65 @@ def hough_transform(img,img_edges):
 	plt.imshow(img, cmap='gray', interpolation='nearest')
 	plt.savefig('img.jpg')
 
+def template_matching():
+	img_rgb = cv.imread('./8.jpg') # opencv reads image in BGR by default.
+	img_rgb = cv.cvtColor(img_rgb, cv.COLOR_BGR2RGB) # convert to standard RGB for matplotlib deafult.
+	
+	# get rgb pixels color count
+	red = img_rgb[:,:,0]
+	green = img_rgb[:,:,1]
+	blue = img_rgb[:,:,2] 
+
+	red_count = np.sum(red)
+	green_count = np.sum(green)
+	blue_count = np.sum(blue)
+
+	# detecting the color of the card
+	if(red_count>green_count):
+		color = 'red'
+	else:
+		color = 'black'
+
+	img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY) # convert to grayscale
+
+	# detect suit 
+	if(color=='red'):
+		template = cv.imread('./heart_template.jpg',0)
+		w, h = template.shape[::-1]
+		res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+		threshold = 0.95
+		loc = np.where( res >= threshold)
+		if(len(loc)>0):
+			suit = 'heart'
+		else:
+			suit = 'diamond'
+	else:
+		template = cv.imread('./spade_template.jpg',0)
+		w, h = template.shape[::-1]
+		res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+		threshold = 0.95
+		loc = np.where( res >= threshold)
+		if(len(loc)>0):
+			suit = 'spade'
+		else:
+			suit = 'club'
+
+	# detect rank
+
+
+	for pt in zip(*loc[::-1]):
+		cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+	plt.imshow(img_rgb)
+	plt.savefig('img4.jpg')
+
 if (__name__ == '__main__'):
-	image, gray = read_gray_image('./2.jpg')
+	#template_matching()
+	image, gray = read_gray_image('./king.jpg')
 	thresholded = threshold_otsu_method(gray)
-	#edges = extract_contour(thresholded)
-	edges = contours(thresholded)
+	edges = extract_contour(thresholded)
+	#edges = contours(thresholded)
 	#prob_hough_transform(image, edges)
 	#hough_transform(image, edges)
+
+
 	
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from functions import *
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
+train_suits = Cards.load_suits( path + '/Card_Imgs/')
