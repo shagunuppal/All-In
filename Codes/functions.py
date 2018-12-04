3a1f868a8965003b7213955eb9ec4ef51e691cc8
Sarthak Bhagat <sarthak16189@iiitd.ac.in>
2018-12-04 21:35:09 +0530

    final

--- Codes/functions.py
+++ Codes/functions.py
@@ -37,9 +37,9 @@ def prob_hough_transform(img, img_edges):
 	for n in range(len(lines)):
 		for x1,y1,x2,y2 in lines[n]:
 			cv.line(img_rgb,(x1,y1),(x2,y2),(0,255,0),2)
-	plt.imshow(img_rgb, cmap='gray', interpolation='nearest')
-	plt.savefig('./results/img_hough_prob.png')
-	plt.close()
+	#plt.imshow(img_rgb, cmap='gray', interpolation='nearest')
+	#plt.savefig('./results/img_hough_prob.png')
+	#plt.close()
 
 def hough_transform(img,img_edges):	
 	line_array = []
@@ -55,13 +55,13 @@ def hough_transform(img,img_edges):
 				if(indexes[v1]==0):
 					v = lines[v1]
 					if(v1!=r1):
-						if(abs(r[0][0]-v[0][0])<10 and r[0][1]-v[0][1]==0):
+						if(abs(r[0][0]-v[0][0])<5 and r[0][1]-v[0][1]==0):
 							if(r[0][0]>v[0][0]):
 								indexes[r1] = 1
 							else:
 								indexes[v1] = 1
 	lines1 = []
-	print('Length:', len(lines))
+	#print('Length:', len(lines))
 	for i in range(len(indexes)):
 		if(indexes[i]==0):
 			lines1.append(lines[i])
@@ -84,7 +84,6 @@ def hough_transform(img,img_edges):
 	plt.imshow(img_rgb, cmap='gray', interpolation='nearest')
 	plt.savefig('./results/img_hough.png')
 	plt.close()
-	print(len(line_array))
 	return line_array
 
 def find_corners(img, lines):
@@ -144,49 +143,51 @@ def plot_points(img, points, name):
 	green = [0, 255, 0]
 	img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 	x, y = img_rgb.shape[0], img.shape[1]
-	print(len(points))
+	#print('points:', points)
 	for i in points:
 		if(i[0]<y and i[1]<x):
-			cv.circle(img_rgb,(int(i[0]),int(i[1])),10,(50,0,250),-1) 
+			cv.circle(img_rgb,(int(i[0]),int(i[1])),5,(50,0,250),-1) 
 	plt.imshow(img_rgb, cmap='gray', interpolation='nearest')
 	plt.savefig('./results/'+name+'.png')
 	plt.close()
 
 def determine_rank(extracted_card):
 	#possible_ranks = ['./A_template.png', './2_template.png', './3_template.png', './4_template.png', './5_template.png', './6_template.png', './7_template.png', './8_template.png', './9_template.png', './10_template.png', './J_template.png''./Q_template.png', './K_template.png']
-	possible_ranks = ['./2_template.png', './8_template.png']
-	
+	possible_ranks = ['./templates/ranks/2_black.png', './templates/ranks/3_red.png', './templates/ranks/3_red1.jpeg', './templates/ranks/7_red.png', './templates/ranks/8_red.jpg', './templates/ranks/9_black.png', './templates/ranks/10_red.png', './templates/ranks/A_red.png', './templates/ranks/4_black.png', './templates/ranks/7_red1.png', './templates/ranks/K_red.png', './templates/ranks/8_black.png', './templates/ranks/A_black1.png']
 	img_rgb = cv.imread(extracted_card)
 	img_rgb = cv.cvtColor(img_rgb, cv.COLOR_BGR2RGB)
 	img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 	rank_ = 0
-	for i in possible_ranks:
-		rank_ = template_matching_rank(img_gray, img_rgb, i)
-		if(rank_>0):
-			rank = i.split('/')[1].split('_')[0]
-			print('Rank of the card is : ', rank)
-			break
-
+	rank_ = template_matching_rank(img_gray, img_rgb, possible_ranks)
+	#for i in possible_ranks:
+		#rank_ = template_matching_rank(img_gray, img_rgb, i)
+		#if(rank_>0):
+	#print("=============================================================", rank_)
+	rank = rank_.split('/')[3].split('_')[0]
+	print('Rank of the card is : ', rank)
+	return rank
+	
 def determine_suit(extracted_card):
-	red_possible_suits = ['./templates/suits/heart/heart.png', './templates/suits/heart/heart1.png', './templates/suits/heart/heart2.png']
-	black_possible_suits = ['./templates/suits/spade/spade.jpg', './templates/suits/spade/spade1.jpg', './templates/suits/spade/spade2.png']
-
+	red_possible_suits = ['./templates/suits/heart/heart.png', './templates/suits/heart/heart1.png', './templates/suits/heart/heart2.png', './templates/suits/heart/heart3.png']
+	black_possible_suits = ['./templates/suits/spade/spade.jpg', './templates/suits/spade/spade1.jpg', './templates/suits/spade/spade2.png', './templates/suits/spade/spade3.png', './templates/suits/spade/spade4.png']
 	img_rgb = cv.imread(extracted_card) # opencv reads image in BGR by default.
 	img_rgb = cv.cvtColor(img_rgb, cv.COLOR_BGR2RGB) # convert to standard RGB for matplotlib deafult.
 	# get rgb pixels color count
 	red = img_rgb[:,:,0]
+	#red = red[10:, 10:]
+	#print("size", red.shape)
 	green = img_rgb[:,:,1]
 	blue = img_rgb[:,:,2] 
 	red_count = np.sum(red)
 	green_count = np.sum(green)
 	blue_count = np.sum(blue)
-	print(red_count, green_count, blue_count)
+	#print("Count : ", red_count, green_count, blue_count)
 	# detecting the color of the card
 	if(red_count>green_count):
 		color = 'red'
 	else:
 		color = 'black'
-
+	#color = 'black'
 	img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY) # convert to grayscale
 	if(color=='red'):
 		suit = template_matching_suit(img_gray, img_rgb, red_possible_suits, color)
@@ -195,16 +196,19 @@ def determine_suit(extracted_card):
 
 	print('Color of the card is : ', color)
 	print('Suit of the card is : ', suit)
+	return suit
 
 
-def template_matching_suit(image, img_rgb, template, color):
+def template_matching_suit(image, img_rgb, templates, color):
 	# detect suit
 	max_length = 0
-	print(template)
-	for template_ in template:
+	#print(template)
+	for template_ in templates:
 		suit = 0
+		#print(template_)
 		template = cv.imread(template_,0)
-		print(template.shape)
+		#print(template.shape)
+		#print(template)
 		w, h = template.shape[::-1]
 		res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
 		threshold = 0.8
@@ -224,24 +228,33 @@ def template_matching_suit(image, img_rgb, template, color):
 	if(max_length!=0):
 		for pt in zip(*loc[::-1]):
 			cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-		plt.imshow(img_rgb)
-		plt.savefig('img_with_suit.png')
-		plt.close()
-	return suit
+		#plt.imshow(img_rgb)
+		#plt.savefig('img_with_suit.png')
+		#plt.close()
+	return suit[0]
 
-def template_matching_rank(image, img_rgb, template_):
+def template_matching_rank(image, img_rgb, templates):
 	# detect rank
-	template = cv.imread(template_,0)
-	w, h = template.shape[::-1]
-	res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
-	threshold = 0.95
-	loc = np.where( res >= threshold)
-	for pt in zip(*loc[::-1]):
-		cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-	plt.imshow(img_rgb)
-	plt.savefig('img_with_rank.png')
-	plt.close()
-	return len(loc[0])
+	max_length = 0
+	name = ""
+	for template_ in templates:
+		rank = 0
+		template = cv.imread(template_,0)
+		w, h = template.shape[::-1]
+		res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
+		threshold = 0.95
+		loc1 = np.where( res >= threshold)
+		if(len(loc1[0])>max_length):
+			max_length = len(loc1[0])
+			loc = loc1
+			name = template_
+	if(max_length!=0):
+		for pt in zip(*loc[::-1]):
+			cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+		#plt.imshow(img_rgb)
+		#plt.savefig('img_with_rank.png')
+		#plt.close()
+	return name
 
 def perspective_transform(img,points):
 	# segregate points
@@ -260,8 +273,6 @@ def perspective_transform(img,points):
 	y2 = y[k[1]]
 	y2.sort(key=lambda x: x[0])
 
-	print(y1)
-	print(y2)
 	index = []
 
 	img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -269,6 +280,8 @@ def perspective_transform(img,points):
 		loop_increment = 2
 	else:
 		loop_increment = 1 
+	#print('y1:', y1)
+	#print('y2:', y2)
 	for i in range(0, (int)(len(y1)), loop_increment):
 		if(i<len(y1) and i+1<len(y1)):
 			pts1 = np.float32([y1[i], y2[i], y1[i+1], y2[i+1]])
@@ -282,15 +295,22 @@ def perspective_transform(img,points):
 	return index
 
 if (__name__ == '__main__'):
-	i = 'C:\\Users\\User\\Desktop\\All-In-master\\Codes\\test_cards\\card13.jpg'
+	i = './test_cards/card21.jpeg'
 	image, gray = read_gray_image(i)
 	thresholded = threshold_otsu_method(gray)
 	edges = extract_contour(thresholded)
 	lines = hough_transform(image, edges)
 	points, pts_1, pts_2 = find_corners(image, lines)
-	#prob_hough_transform(image, edges)
 	plot_points(image, points, 'points')
 	index = perspective_transform(image, points)
+	suit = []
+	rank = []
 	for i in index:
-		determine_suit('./results/perspective/'+i+'.png')
-		#determine_rank('./2.png')
+		s = determine_suit('./results/perspective/'+i+'.png')
+		r = determine_rank('./results/perspective/'+i+'.png')
+		suit.append(s)
+		rank.append(r)
+	cards = []
+	for i in range(len(suit)):
+		cards.append(rank[i] + suit[i])
+	print ('CARDS are : ', cards)
